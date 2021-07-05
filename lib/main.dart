60d95cc0148fff34c8499deb388b793f90f90921
gurum77 <hileejaeho@gmail.com>
2021-07-06 00:50:44 +0900

    working

--- lib/main.dart
+++ lib/main.dart
@@ -47,16 +47,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Uint8List? _imageData;
-  bool _cropping = false;
-  final GlobalKey<ExtendedImageEditorState> imageKey =
-      GlobalKey<ExtendedImageEditorState>();
+  int _imageWidth = 0;
+  int _imageHeight = 0;
 
   @override
   Widget build(BuildContext context) {
-    _cropping = false;
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -67,116 +62,23 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             _imageData == null
-                ? Text('')
-                : Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(20),
-                        width: width * 0.7,
-                        height: height - 300,
-                        child: ExtendedImage.memory(
-                          _imageData!,
-                          extendedImageEditorKey: imageKey,
-                          mode: ExtendedImageMode.editor,
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        width: width * 0.2,
-                        height: height - 300,
-                        child: Column(
-                          children: [
-                            Container(
-                              width: width * 0.2,
-                              child: TextField(
-                                  onChanged: (text) {},
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                      labelText: "Width(px)",
-                                      icon: Icon(Icons
-                                          .swap_horizontal_circle_outlined))),
-                            ),
-                            Container(
-                              width: width * 0.2,
-                              child: TextField(
-                                  onChanged: (text) {},
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                      labelText: "Height(px)",
-                                      icon: Icon(Icons
-                                          .swap_vertical_circle_outlined))),
-                            ),
-                            SizedBox(height: 20),
-                            Container(
-                              width: width * 0.15,
-                              child: ElevatedButton(
-                                  onPressed: () {
-                                    imageKey.currentState!.rotate(right: true);
-                                  },
-                                  child: Text('Rotate')),
-                            ),
-                            SizedBox(height: 20),
-                            Container(
-                              width: width * 0.15,
-                              child: ElevatedButton(
-                                  onPressed: () {
-                                    imageKey.currentState!.flip();
-                                  },
-                                  child: Text('Flip')),
-                            ),
-                            SizedBox(height: 20),
-                            Container(
-                              width: width * 0.15,
-                              child: ElevatedButton(
-                                  onPressed: () {
-                                    _cropImage();
-                                  },
-                                  child: Text('Download')),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-            SizedBox(height: 30),
-            _imageData == null
-                ? Text('')
-                : Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Spacer(),
-                      Spacer(),
-                      ElevatedButton(
-                          onPressed: () {
-                            imageKey.currentState!.rotate(right: true);
-                          },
-                          child: Text('Rotate')),
-                      Spacer(),
-                      ElevatedButton(
-                          onPressed: () {
-                            imageKey.currentState!.flip();
-                          },
-                          child: Text('Flip')),
-                      Spacer(),
-                      ElevatedButton(
-                          onPressed: () {
-                            _cropImage();
-                          },
-                          child: Text('Save')),
-                      Spacer(),
-                      Spacer(),
-                    ],
-                  ),
+                ? Text('Select image file to resize.')
+                : ImageResizer(
+                    imageData: _imageData,
+                    imageWidth: _imageWidth,
+                    imageHeight: _imageHeight),
             SizedBox(height: 30),
             ElevatedButton(
                 onPressed: () {
-                  FilePicker.platform.pickFiles().then((value) {
+                  FilePicker.platform
+                      .pickFiles(type: FileType.image)
+                      .then((value) {
                     if (value != null && value.files.length > 0) {
                       setState(() {
                         _imageData = value.files[0].bytes;
+                        var image = decodeImage(_imageData!);
+                        _imageWidth = image!.width;
+                        _imageHeight = image.height;
                       });
                     }
                   });
@@ -187,6 +89,32 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+class ImageResizer extends StatefulWidget {
+  ImageResizer(
+      {Key? key,
+      required this.imageData,
+      required this.imageWidth,
+      required this.imageHeight})
+      : super(key: key) {
+    ratio = imageWidth / imageHeight;
+  }
+
+  Uint8List? imageData;
+  int imageWidth = 0;
+  int imageHeight = 0;
+  double ratio = 1;
+
+  @override
+  _ImageResizerState createState() => _ImageResizerState();
+}
+
+class _ImageResizerState extends State<ImageResizer> {
+  bool _cropping = false;
+
+  final GlobalKey<ExtendedImageEditorState> imageKey =
+      GlobalKey<ExtendedImageEditorState>();
 
   Future<void> _cropImage() async {
     if (_cropping) {
@@ -197,18 +125,18 @@ class _MyHomePageState extends State<MyHomePage> {
     try {
       _cropping = true;
 
-      Uint8List? imageData;
+      Uint8List? newImageData;
 
-      imageData =
+      newImageData =
           await cropImageDataWithDartLibrary(state: imageKey.currentState!);
 
-      // 테스트 : 이미지 크기를 100 x 100으로 변경하는 코드
-      var image = decodeImage(imageData!);
-      var newImage = copyResize(image!, height: 100, width: 100);
-      imageData = Uint8List.fromList(encodeJpg(newImage));
+      var image = decodeImage(newImageData!);
+      var newImage = copyResize(image!,
+          height: widget.imageHeight, width: widget.imageWidth);
+      newImageData = Uint8List.fromList(encodeJpg(newImage));
       //
 
-      await ImageSaver.save('extended_image_cropped_image.jpg', imageData);
+      await ImageSaver.save('extended_image_cropped_image.jpg', newImageData);
     } catch (e, stack) {
       msg = 'save failed: $e\n $stack';
       _cropping = false;
@@ -216,5 +144,94 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     _cropping = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _cropping = false;
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.all(20),
+          width: MediaQuery.of(context).size.width * 0.7,
+          height: MediaQuery.of(context).size.height - 300,
+          child: ExtendedImage.memory(
+            widget.imageData!,
+            extendedImageEditorKey: imageKey,
+            mode: ExtendedImageMode.editor,
+            fit: BoxFit.contain,
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: MediaQuery.of(context).size.height - 300,
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.2,
+                child: TextFormField(
+                  key: Key(widget.imageWidth.toString()),
+                    onFieldSubmitted: (text) {
+                      setState(() {
+                        // 폭을 변경하면 높이를 비율에 맞게 조정
+                        widget.imageWidth = int.parse(text.toString());
+                        double w = widget.imageWidth as double;
+                        double newHeight = w / widget.ratio;
+                        var nh = newHeight.floor();
+                        widget.imageHeight = nh;
+                      });
+                    },
+                    keyboardType: TextInputType.number,
+                    initialValue: widget.imageWidth.toString(),
+                    decoration: InputDecoration(
+                        labelText: "Width(px)",
+                        icon: Icon(Icons.swap_horizontal_circle_outlined))),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.2,
+                child: TextFormField(
+                  key: Key(widget.imageHeight.toString()),
+                    onChanged: (text) {},
+                    keyboardType: TextInputType.number,
+                    initialValue: widget.imageHeight.toString(),
+                    decoration: InputDecoration(
+                        labelText: "Height(px)",
+                        icon: Icon(Icons.swap_vertical_circle_outlined))),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                child: ElevatedButton(
+                    onPressed: () {
+                      imageKey.currentState!.rotate(right: true);
+                    },
+                    child: Text('Rotate')),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                child: ElevatedButton(
+                    onPressed: () {
+                      imageKey.currentState!.flip();
+                    },
+                    child: Text('Flip')),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                child: ElevatedButton(
+                    onPressed: () {
+                      _cropImage();
+                    },
+                    child: Text('Download')),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
