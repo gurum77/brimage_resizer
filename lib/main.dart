9434bc929721ee124b7ba53f476c087fe4e0bac2
gurum77 <hileejaeho@gmail.com>
2021-07-07 01:04:38 +0900

    presets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:html';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -40,6 +41,112 @@ class _MyHomePageState extends State<MyHomePage> {
   Uint8List? _imageData;
   int _imageWidth = 0;
   int _imageHeight = 0;
+  var _presets = new Map<String, List>();
+
+// https://blog.hootsuite.com/social-media-image-sizes-guide/#Instagram_image_sizes
+  void _initPresets() {
+    var _instagram = [
+      SizeInfo.make('Profile photo', Size(320, 320)),
+      SizeInfo.make('Feed photo(Landscape)', Size(1000, 566)),
+      SizeInfo.make('Feed photo(Portrait)', Size(1000, 1350)),
+      SizeInfo.make('Feed photo(square)', Size(1080, 1080)),
+      SizeInfo.make('Stories', Size(1080, 1920)),
+      SizeInfo.make('Thumbnails', Size(161, 161)),
+      SizeInfo.make('Portrait', Size(1080, 1350))
+    ];
+
+     var _twitter = [
+      SizeInfo.make('Profile photo', Size(400, 400)),
+      SizeInfo.make('Header Photo', Size(1500, 500)),
+      SizeInfo.make('In-Stram photo', Size(1600, 1900)),
+      SizeInfo.make('Card image', Size(1200, 1200)),
+      SizeInfo.make('Fleets', Size(1080, 1920))
+    ];
+
+    var _facebook = [
+      SizeInfo.make('Profile photo', Size(170, 170)),
+      SizeInfo.make('Cover photo1', Size(851, 315)),
+      SizeInfo.make('Cover photo2', Size(1200, 628)),
+      SizeInfo.make('Post/Timeline photo', Size(1200, 630)),
+      SizeInfo.make('Stories', Size(1080, 1920)),
+      SizeInfo.make('Ads', Size(1080, 1080))
+    ];
+
+    var _linkedin = [
+      SizeInfo.make('Profile photo', Size(400, 400)),
+      SizeInfo.make('Profile cover photo', Size(1584, 396)),
+      SizeInfo.make('Blog post link image', Size(1200, 627)),
+      SizeInfo.make('Sharing a link in an update', Size(1200, 627)),
+      SizeInfo.make('Stories', Size(1080, 1920)),
+      SizeInfo.make('Company logo', Size(300, 300)),
+      SizeInfo.make('Page cover', Size(1128, 191)),
+      SizeInfo.make('Life tab main image', Size(1128, 376)),
+      SizeInfo.make('Life tab custom modules', Size(502, 282)),
+      SizeInfo.make('Life tab company photo', Size(900, 600)),
+      SizeInfo.make('Square logo', Size(60, 60)),
+      SizeInfo.make('Company logo size for ads', Size(100, 100)),
+      SizeInfo.make('Spotlight ads logo', Size(100, 100)),
+      SizeInfo.make('Spotlight ads custom background', Size(300, 250)),
+      SizeInfo.make('Sponsored content images', Size(1200, 627)),
+      SizeInfo.make('Sponsored content carousel', Size(1080, 1080))
+    ];
+
+    var _pinterest = [
+      SizeInfo.make('Profile photo', Size(365, 165)),
+      SizeInfo.make('Profile cover photo', Size(800, 450)),
+      SizeInfo.make('Pins', Size(1000, 1500)),
+      SizeInfo.make('Story pins', Size(1080, 1920)),
+      SizeInfo.make('Fleets', Size(1080, 1920)),
+      SizeInfo.make('Collections pins1', Size(1000, 1000)),
+      SizeInfo.make('Collections pins2', Size(1000, 1500)),
+      SizeInfo.make('App install ads', Size(1000, 1500)),
+      SizeInfo.make('Carousel pins and ads1', Size(1000, 1000)),
+      SizeInfo.make('Carousel pins and ads2', Size(1000, 1500)),
+      SizeInfo.make('Shopping ads', Size(1000, 1500)),
+      
+    ];
+
+    var _googlePlayStore = [
+      SizeInfo.make('App icon', Size(512, 512)),
+      SizeInfo.make('Graphics image', Size(1024, 500)),
+      SizeInfo.make('Screen shot(Min,L)', Size(568, 320)),
+      SizeInfo.make('Screen shot(Min,P)', Size(320, 568)),
+      SizeInfo.make('Screen shot(Max,L)', Size(3840, 2160)),
+      SizeInfo.make('Screen shot(Max,P)', Size(2160, 3840))
+    ];
+
+    _presets['Instagram best fit'] = _instagram;
+    _presets['Twitter best fit'] = _twitter;
+    _presets['Face best fit'] = _facebook;
+    _presets['Linkedin best fit'] = _linkedin;
+    _presets['Google play store'] = _googlePlayStore;
+    _presets['Pinterest'] = _pinterest;
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initPresets();
+  }
+  // preset을 json에서 로딩하는건 공부하고 나서 하자.
+  void _loadPresets() {
+    rootBundle.loadString('assets/presets.json').then((value) {
+      Map jsonResult = jsonDecode(value);
+      jsonResult.forEach((key, value) {
+        List<SizeInfo> newPreset = [];
+        List list = value;
+        list.forEach((element) {
+          SizeInfo si = SizeInfo(
+              title: element['name'],
+              size:
+                  Size(double.parse(element['w']), double.parse(element['h'])));
+          newPreset.add(si);
+        });
+        _presets[key] = newPreset;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +165,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   : ImageResizer(
                       imageData: _imageData,
                       imageWidth: _imageWidth,
-                      imageHeight: _imageHeight),
+                      imageHeight: _imageHeight,
+                      presets: _presets,
+                    ),
               SizedBox(height: 30),
               ElevatedButton(
                   onPressed: () {
@@ -84,18 +193,27 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class SizeInfo {
+  String title = '';
+  Size size = Size(0, 0);
+  SizeInfo({required this.title, required this.size});
+  SizeInfo.make(String title, Size size) {
+    this.title = title;
+    this.size = size;
+  }
+}
+
 class ImageResizer extends StatefulWidget {
   ImageResizer(
       {Key? key,
       required this.imageData,
       required this.imageWidth,
-      required this.imageHeight})
+      required this.imageHeight,
+      required this.presets})
       : super(key: key) {
     ratio = imageWidth / imageHeight;
     originSize = Size(imageWidth as double, imageHeight as double);
-
-    presets['Google play store'] = googlePlayStorePresets;
-    presets['Itch.io'] = itchIoPresets;
+    selectedPresetName = this.presets.keys.elementAt(0);
   }
 
   Uint8List? imageData;
@@ -105,17 +223,17 @@ class ImageResizer extends StatefulWidget {
   bool lockedAspectRatio = true;
   Size originSize = Size(0, 0);
   Size presetSize = Size(0, 0);
-  var googlePlayStorePresets = [
-    Size(512, 512),
-    Size(1024, 500),
-    Size(568, 320),
-    Size(3840, 2160),
-    Size(320, 568),
-    Size(2160, 3840)
-  ];
-  var itchIoPresets = [Size(315, 250), Size(630, 500)];
   var presets = new Map<String, List>();
-  String selectedPresetName = 'Google play store';
+  // var googlePlayStorePresets = [
+  //   Size(512, 512),
+  //   Size(1024, 500),
+  //   Size(568, 320),
+  //   Size(3840, 2160),
+  //   Size(320, 568),
+  //   Size(2160, 3840)
+  // ];
+
+  String selectedPresetName = '';
 
   @override
   _ImageResizerState createState() => _ImageResizerState();
@@ -207,18 +325,29 @@ class _ImageResizerState extends State<ImageResizer> {
                 },
               ),
               Container(
-                width: MediaQuery.of(context).size.width * 0.2,
-                height: 200,
+                width: MediaQuery.of(context).size.width * 0.18,
+                height: 300,
                 child: ListView.builder(
                     key: Key(widget.selectedPresetName),
                     itemCount:
                         widget.presets[widget.selectedPresetName]!.length,
                     itemBuilder: (context, position) {
-                      print(widget.presets[widget.selectedPresetName]!.length
-                          .toString());
-                      return Text(widget.presets[widget.selectedPresetName]!
-                          .elementAt(position)
-                          .toString());
+                      return ListTile(
+                        title: Text(_getPresetListTileText(position)),
+                        leading: Icon(Icons.photo_size_select_large_outlined),
+                        onTap: () {
+                          setState(() {
+                            Size size = widget
+                                .presets[widget.selectedPresetName]!
+                                .elementAt(position)
+                                .size;
+                            widget.imageWidth = size.width.toInt();
+                            widget.imageHeight = size.height.toInt();
+                            widget.ratio =
+                                widget.imageWidth / widget.imageHeight;
+                          });
+                        },
+                      );
                     }),
               ),
               Container(
@@ -345,5 +474,15 @@ class _ImageResizerState extends State<ImageResizer> {
     }
 
     return items;
+  }
+
+  String _getPresetListTileText(int position) {
+    String str;
+    Size size =
+        widget.presets[widget.selectedPresetName]!.elementAt(position).size;
+        String title = widget.presets[widget.selectedPresetName]!.elementAt(position).title;
+    str = "$title - ${size.width} x ${size.height}";
+
+    return str;
   }
 }
